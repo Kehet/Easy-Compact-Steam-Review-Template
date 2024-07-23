8873eb084abeb4687c19e21f860a8445b408017b
Kehet <382965-kehet@users.noreply.gitlab.com>
2024-07-23 03:06:07 +0300

    First version

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,140 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+import QuestionBlock from '@/components/QuestionBlock.vue'
+
+const topics = [
+  {
+    title: 'Story',
+    help: "Is the plot engaging and well-constructed?  Are there any moments where the game feels too slow or rushed? Do all plot points and events make sense within the story's world? Are the characters well-developed and multi-dimensional? What are the central themes of the story? How emotionally engaging is the story? How well-written is the dialogue and narration?"
+  },
+  {
+    title: 'Gameplay',
+    help: 'How intuitive and responsive are the controls? How well-designed are the levels or environments? Do they provide a good balance of challenge and reward?'
+  },
+  {
+    title: 'Graphics',
+    help: 'What kind of graphics style does the game have? Does the game look realistic? Does the game feel smooth and satisfying to play? Is the user interface intuitive and easy to navigate?'
+  },
+  {
+    title: 'Audio',
+    help: 'Did you like the background music? Sound effects? Sounds of the environment? Did you play without music?'
+  },
+  {
+    title: 'Replay value',
+    help: 'Does the game offer different modes or challenges? What incentives are there for replaying the game (e.g., multiple endings, unlockables)?'
+  },
+  {
+    title: 'Difficulty',
+    help: 'Was the game too easy? Too difficult? Just perfect? Is it possible to adjust the difficulty level?'
+  },
+  {
+    title: 'Price',
+    help: 'Is the price justified? Did you buy the game on sale?'
+  },
+  {
+    title: 'Accessibility',
+    help: 'What accessibility options are available (e.g., subtitles, colorblind modes)? How inclusive is the game for players with different needs? Were there big spiders or other triggers?'
+  }
+]
+
+const summary = ref('')
+
+const form = ref([
+  { rating: 3, notes: '' },
+  { rating: 3, notes: '' },
+  { rating: 3, notes: '' },
+  { rating: 3, notes: '' },
+  { rating: 3, notes: '' },
+  { rating: 3, notes: '' },
+  { rating: 3, notes: '' },
+  { rating: 3, notes: '' }
+])
+
+const generateRating = (rating) => {
+  let out = []
+  for (let i = 0; i < rating; i++) {
+    out.push('⭐')
+  }
+  for (let i = 0; i < 5 - rating; i++) {
+    out.push('➖')
+  }
+  return out.join('')
+}
+
+const generateText = () => {
+  let output = summary.value + '\n\n[table noborder=1]\n'
+
+  for (const i in topics) {
+    if (form.value[i].rating == null) continue
+
+    output += '[tr]\n'
+    output += `[td]${topics[i].title}[/td]\n`
+    output += `[td]${generateRating(form.value[i].rating)}[/td]\n`
+    output += `[td]${form.value[i].notes}[/td]\n`
+    output += '[/tr]\n'
+  }
+
+  output += '[/table]'
+
+  console.log(output)
+
+  navigator.clipboard.writeText(output).then(
+    () => {
+      alert('Review copied to clipboard')
+    },
+    () => {
+      alert('Failed to copy')
+    }
+  )
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="max-w-3xl mx-auto my-12 text-gray-600 dark:text-gray-400">
+    <div class="flex flex-col text-center w-full mb-12">
+      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900 dark:text-white">
+        Easy Compact Steam Review Template
+      </h1>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+        Want to be lazy but hate those checkbox reviews? Well this should generate bit more compact
+        review template. I also added some helper questions because I think words are more helpful
+        than few emojis.
+      </p>
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="flex flex-col gap-y-12">
+      <div>
+        <h2 class="grow text-2xl mb-2 text-gray-900 dark:text-white">Summary</h2>
+
+        <p class="mb-3">
+          How enjoyable is the overall gameplay experience? What sets this game apart from others in
+          its genre? What are the most fun or engaging parts of the game?
+        </p>
+
+        <textarea
+          v-model="summary"
+          rows="4"
+          class="w-full bg-gray-100 dark:bg-gray-800 bg-opacity-50 rounded border border-gray-300 dark:border-gray-700 focus:border-yellow-500 focus:bg-white dark:focus:bg-gray-900 focus:ring-2 focus:ring-yellow-200 dark:focus:ring-yellow-900 text-base outline-none text-gray-700 dark:text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+        ></textarea>
+      </div>
+
+      <QuestionBlock
+        v-for="(topic, i) in topics"
+        :key="i"
+        :title="topic.title"
+        :help="topic.help"
+        v-model:rating="form[i].rating"
+        v-model:notes="form[i].notes"
+      />
+
+      <div class="p-2 w-full">
+        <button
+          @click="generateText"
+          class="flex mx-auto text-white dark:text-black bg-yellow-500 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-lg"
+        >
+          Copy review
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
---
+++ src/components/QuestionBlock.vue
@@ -0,0 +1,54 @@
+<script setup>
+import StarRating from '@/components/StarRating.vue'
+
+defineProps({
+  title: String,
+  help: String
+})
+
+const rating = defineModel('rating')
+const notes = defineModel('notes')
+</script>
+
+<template>
+  <div class="p-2 w-full">
+    <div class="flex flex-col gap-y-3">
+      <div class="flex justify-between">
+        <h2 class="grow text-2xl text-gray-900 dark:text-white">
+          {{ title }}
+        </h2>
+
+        <button
+          v-if="rating !== null"
+          @click="rating = null"
+          class="flex mx-auto text-white dark:text-black bg-yellow-500 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded"
+        >
+          Hide
+        </button>
+
+        <button
+          v-else
+          @click="rating = 3"
+          class="flex mx-auto text-white dark:text-black bg-yellow-500 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded"
+        >
+          Show
+        </button>
+      </div>
+
+      <StarRating v-show="rating !== null" v-model="rating" />
+
+      <p v-if="rating !== null && help">
+        {{ help }}
+      </p>
+
+      <textarea
+        v-show="rating !== null"
+        v-model="notes"
+        rows="4"
+        class="w-full bg-gray-100 dark:bg-gray-800 bg-opacity-50 rounded border border-gray-300 dark:border-gray-700 focus:border-yellow-500 focus:bg-white dark:focus:bg-gray-900 focus:ring-2 focus:ring-yellow-200 dark:focus:ring-yellow-900 text-base outline-none text-gray-700 dark:text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+      ></textarea>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
